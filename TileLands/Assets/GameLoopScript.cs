cf1ad215818df90a7382d1c4ffbb01d2b0cc393b
dansyron <Syron@Dannys-MacBook-Air.local>
2017-02-01 14:24:04 -0700

    added procedural sunlight

--- TileLands/Assets/GameLoopScript.cs
+++ TileLands/Assets/GameLoopScript.cs
@@ -5,13 +5,30 @@ using UnityEngine;
 public class GameLoopScript : MonoBehaviour {
 
 	GameObject startTile;
+	GameObject OceanPlane;
+	GameObject sunLight;
+
 	public List<GameObject> solutionList;
+
+	float randomLightangle;
 
 	// Use this for initialization
 	void Start () {
 
+		//calculate random sunlight angle.
+		randomLightangle = (float)(Random.Range(-80, 81));
+
 		//create the new solution list
 		solutionList = new List<GameObject> ();
+
+		sunLight = (GameObject)Instantiate (Resources.Load ("Lights/SunLightObject"), Vector3.zero, Quaternion.identity);
+		sunLight.transform.Rotate(new Vector3( 90f, 0, 0));
+
+
+		OceanPlane = Resources.Load<GameObject> ("OceanPlane");
+
+		//create ocean plane or other object.
+		Instantiate (OceanPlane, Vector3.zero, Quaternion.identity);
 
 		//create the new start tile
 		startTile = (GameObject)(Instantiate (Resources.Load ("BasicTile"), Vector3.zero, Quaternion.identity));
@@ -20,6 +37,9 @@ public class GameLoopScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
+
+
 	}
 
 
---
+++ TileLands/Assets/scripts/RandomSunLight.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSunLight : MonoBehaviour {
+
+	int randomLightAngle;
+
+	// Use this for initialization
+	void Start () {
+
+	
+		randomLightAngle = Random.Range (-80, 81);
+
+		transform.Rotate(new Vector3(randomLightAngle, 0, 0));
+
+		if (Mathf.Abs (randomLightAngle) > 50) {
+			GetComponent<Light> ().intensity = 2.5f;
+			GetComponent<Light> ().color = new Color (.73f, .3f, 0);
+
+		} else if (Mathf.Abs (randomLightAngle) <= 50 && Mathf.Abs (randomLightAngle) > 30) {
+			GetComponent<Light> ().intensity = 1f;
+			GetComponent<Light> ().color = new Color (.1f, .05f, .6f);
+		} else {
+			GetComponent<Light> ().intensity = .80f;
+			GetComponent<Light> ().color = new Color (1f, .85f, .75f);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
