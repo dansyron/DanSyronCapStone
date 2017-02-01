1c72ea9b3e15f9b713e91e384ebb25b04455a566
dansyron <Syron@Dannys-MacBook-Air.local>
2017-01-31 21:54:46 -0700

    changed files

--- TileLands/Assets/scripts/PuzzleGeneratorScript.cs
+++ TileLands/Assets/scripts/PuzzleGeneratorScript.cs
@@ -30,7 +30,7 @@ public class PuzzleGeneratorScript : MonoBehaviour {
 
 		//placeholder evolution levels
 		easyEvolutions = 3;
-		mediumEvolutions = 6;
+		mediumEvolutions = 20;
 		//hardEvolutions = 15;
 		//expertEvolutions = 20;
 
