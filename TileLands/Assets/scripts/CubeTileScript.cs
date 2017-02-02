6b01e68c7063444aef8a1bbdde235acc59f61bb8
dansyron <djsyronmusic@gmail.com>
2017-02-01 19:53:23 -0700

    finished game loop

--- TileLands/Assets/scripts/CubeTileScript.cs
+++ TileLands/Assets/scripts/CubeTileScript.cs
@@ -4,169 +4,176 @@ using UnityEngine;
 
 public class CubeTileScript : MonoBehaviour {
 
-	Vector3 temporaryPosition1;
-	Vector3 temporaryPosition2;
-	Vector3 temporaryPosition3;
-	Vector3 temporaryPosition4;
+    Vector3 temporaryPosition1;
+    Vector3 temporaryPosition2;
+    Vector3 temporaryPosition3;
+    Vector3 temporaryPosition4;
 
 
-	//designated tile skin.
-	GameObject tileSkin;
-	GameObject tileFacade;
-	GameObject rippleEffect;
-	GameObject ripple;
+    //designated tile skin.
+    GameObject tileSkin;
+    GameObject tileFacade;
+    GameObject rippleEffect;
+    GameObject ripple;
 
-	int skinRandomizer;
+    int skinRandomizer;
 
-	// Use this for initialization
-	void Start () 
-	{
-		transform.localScale = new Vector3(1f, 1f, 1f);
+    // Use this for initialization
+    void Start()
+    {
+        transform.localScale = new Vector3(1f, 1f, 1f);
 
-		skinRandomizer = Random.Range (0, 13);
-		rippleEffect = (GameObject)Resources.Load ("Effects/TileRipples");
+        skinRandomizer = Random.Range(0, 13);
+        rippleEffect = (GameObject)Resources.Load("Effects/TileRipples");
 
-		switch (skinRandomizer) {
-		case 0:
-			{
-				tileSkin = (GameObject)Resources.Load ("GameTiles/Forest/GrassTile1");
-			}
-			break;
-		case 1:
-			{
-				tileSkin = (GameObject)Resources.Load ("GameTiles/Forest/GrassTile2");
-			}
-			break;
-		case 2:
-			{
-				tileSkin = (GameObject)(Resources.Load ("GameTiles/Forest/GrassTile3"));
-			}
-			break;
-		case 3:
-			{
-				tileSkin = (GameObject)Resources.Load ("GameTiles/Forest/LampPost1");
-			}
-			break;
-		case 4:
-			{
-				tileSkin = (GameObject)Resources.Load ("GameTiles/Forest/SmallTreeTile1");
-			}
-			break;
-		case 5:
-			{
-				tileSkin = (GameObject)Resources.Load ("GameTiles/Forest/SmallTreeTile2");
-			}
-			break;
-		case 6:
-			{
-				tileSkin = (GameObject)Resources.Load ("GameTiles/Forest/TreeTile1");
-			}
-			break;
-		case 7:
-			{
-				tileSkin = (GameObject)Resources.Load ("GameTiles/Forest/TreeTile2");
-			}
-			break;
-		case 8:
-			{
-				tileSkin = (GameObject)Resources.Load ("GameTiles/Forest/TreeTile3");
-			}
-			break;
-		case 9:
-			{
-				tileSkin = (GameObject)Resources.Load ("GameTiles/Forest/TreeTile4");
-			}
-			break;
-		case 10:
-			{
-				tileSkin = (GameObject)Resources.Load ("GameTiles/Forest/CampSite1");
-			}
-			break;
-		case 11:
-			{
-				tileSkin = (GameObject)Resources.Load ("GameTiles/Forest/Rocks1");
-			}
-			break;
-		case 12:
-			{
-				tileSkin = (GameObject)Resources.Load ("GameTiles/Forest/LargeTreeTile1");
-			}
-			break;
+        switch (skinRandomizer) {
+            case 0:
+                {
+                    tileSkin = (GameObject)Resources.Load("GameTiles/Forest/GrassTile1");
+                }
+                break;
+            case 1:
+                {
+                    tileSkin = (GameObject)Resources.Load("GameTiles/Forest/GrassTile2");
+                }
+                break;
+            case 2:
+                {
+                    tileSkin = (GameObject)(Resources.Load("GameTiles/Forest/GrassTile3"));
+                }
+                break;
+            case 3:
+                {
+                    tileSkin = (GameObject)Resources.Load("GameTiles/Forest/LampPost1");
+                }
+                break;
+            case 4:
+                {
+                    tileSkin = (GameObject)Resources.Load("GameTiles/Forest/SmallTreeTile1");
+                }
+                break;
+            case 5:
+                {
+                    tileSkin = (GameObject)Resources.Load("GameTiles/Forest/SmallTreeTile2");
+                }
+                break;
+            case 6:
+                {
+                    tileSkin = (GameObject)Resources.Load("GameTiles/Forest/TreeTile1");
+                }
+                break;
+            case 7:
+                {
+                    tileSkin = (GameObject)Resources.Load("GameTiles/Forest/TreeTile2");
+                }
+                break;
+            case 8:
+                {
+                    tileSkin = (GameObject)Resources.Load("GameTiles/Forest/TreeTile3");
+                }
+                break;
+            case 9:
+                {
+                    tileSkin = (GameObject)Resources.Load("GameTiles/Forest/TreeTile4");
+                }
+                break;
+            case 10:
+                {
+                    tileSkin = (GameObject)Resources.Load("GameTiles/Forest/CampSite1");
+                }
+                break;
+            case 11:
+                {
+                    tileSkin = (GameObject)Resources.Load("GameTiles/Forest/Rocks1");
+                }
+                break;
+            case 12:
+                {
+                    tileSkin = (GameObject)Resources.Load("GameTiles/Forest/LargeTreeTile1");
+                }
+                break;
 
-		}
-		Initialize ();
-	}
+        }
+        Initialize();
+    }
 
-	void Initialize()
-	{
-		tileFacade = Instantiate (tileSkin, transform.position, Quaternion.identity);
-		tileFacade.transform.SetParent (transform);
+    void Initialize()
+    {
+        tileFacade = Instantiate(tileSkin, transform.position, Quaternion.identity);
+        tileFacade.transform.SetParent(transform);
 
-		int randomRotation = Random.Range (0, 3);
-		tileFacade.transform.Rotate (new Vector3 (0, 90f * randomRotation, 0));
+        int randomRotation = Random.Range(0, 3);
+        tileFacade.transform.Rotate(new Vector3(0, 90f * randomRotation, 0));
 
-		//create ripple on instance
-		ripple = Instantiate (rippleEffect, new Vector3(transform.position.x, -.4f, transform.position.z), Quaternion.identity);
+        //create ripple on instance
+        ripple = Instantiate(rippleEffect, new Vector3(transform.position.x, -.4f, transform.position.z), Quaternion.identity);
 
 
-	}
+    }
 
-	// Update is called once per frame
-	void Update ()
+    // Update is called once per frame
+    void Update()
 
-	{
-		if (transform.localScale.x < 1) 
-		{
-			transform.localScale += new Vector3 (.1f, .1f, .1f);
-		}
-	}
+    {
+        if (transform.localScale.x < 1)
+        {
+            transform.localScale += new Vector3(.1f, .1f, .1f);
+        }
+    }
 
-	void OnTriggerStay(Collider collision) 
-	{
-		if (collision.gameObject.tag == "Tile") {
-			//Destroy (collision.gameObject);
+    void OnTriggerStay(Collider collision)
+    {
+        if (collision.gameObject.tag == "Tile") {
+            //Destroy (collision.gameObject);
 
-			//create ripple on instance
-			ripple = Instantiate (rippleEffect, new Vector3(transform.position.x, -.4f, transform.position.z), Quaternion.identity);
+            //create ripple on instance
+            ripple = Instantiate(rippleEffect, new Vector3(transform.position.x, -.4f, transform.position.z), Quaternion.identity);
 
-			//destroy object
-			Destroy(tileFacade);
-			Destroy (gameObject);
-		}
-	}
+            //remove current cube from list
+            GameLoopScript.instance.activeCubeList.Remove(gameObject);
 
-	void OnMouseDown()
-	{
-		//on mouse click, create a new cube tile
-		if (Input.GetMouseButtonDown (0)) {
+            //destroy object
+            Destroy(tileFacade);
+            Destroy(gameObject);
+        }
+    }
 
-			Vector3 currentPosition = transform.position;
-			temporaryPosition1 = new Vector3 (currentPosition.x + 1, currentPosition.y, currentPosition.z);
-			temporaryPosition2 = new Vector3 (currentPosition.x - 1, currentPosition.y, currentPosition.z);
-			temporaryPosition3 = new Vector3 (currentPosition.x, currentPosition.y, currentPosition.z + 1);
-			temporaryPosition4 = new Vector3 (currentPosition.x, currentPosition.y, currentPosition.z - 1);
+    void OnMouseDown()
+    {
+        //if game is active
+        if (GameManagerScript.instance.gameActive)
+        {
+            //on mouse click, create a new cube tile
+            if (Input.GetMouseButtonDown(0))
+            {
 
-			//instantiate new cubes
-			//Instantiate(Resources.Load("BasicTile"), temporaryPosition1, Quaternion.identity);
-			//Instantiate(Resources.Load("BasicTile"), temporaryPosition2, Quaternion.identity);
-			//Instantiate(Resources.Load("BasicTile"), temporaryPosition3, Quaternion.identity);
-			//Instantiate(Resources.Load("BasicTile"), temporaryPosition4, Quaternion.identity);
-		
+                Vector3 currentPosition = transform.position;
+                temporaryPosition1 = new Vector3(currentPosition.x + 1, currentPosition.y, currentPosition.z);
+                temporaryPosition2 = new Vector3(currentPosition.x - 1, currentPosition.y, currentPosition.z);
+                temporaryPosition3 = new Vector3(currentPosition.x, currentPosition.y, currentPosition.z + 1);
+                temporaryPosition4 = new Vector3(currentPosition.x, currentPosition.y, currentPosition.z - 1);
 
-			GameLoopScript.instance.CreateCube (temporaryPosition1);
+                //instantiate new cubes
+                //Instantiate(Resources.Load("BasicTile"), temporaryPosition1, Quaternion.identity);
+                //Instantiate(Resources.Load("BasicTile"), temporaryPosition2, Quaternion.identity);
+                //Instantiate(Resources.Load("BasicTile"), temporaryPosition3, Quaternion.identity);
+                //Instantiate(Resources.Load("BasicTile"), temporaryPosition4, Quaternion.identity);
 
-			GameLoopScript.instance.CreateCube (temporaryPosition2);
 
-			GameLoopScript.instance.CreateCube (temporaryPosition3);
+                GameLoopScript.instance.CreateCube(temporaryPosition1);
 
-			GameLoopScript.instance.CreateCube (temporaryPosition4);
+                GameLoopScript.instance.CreateCube(temporaryPosition2);
 
-			temporaryPosition1 = currentPosition;
-			temporaryPosition2 = currentPosition;
-			temporaryPosition3 = currentPosition;
-			temporaryPosition4 = currentPosition;
-		}
-			
-	}
+                GameLoopScript.instance.CreateCube(temporaryPosition3);
+
+                GameLoopScript.instance.CreateCube(temporaryPosition4);
+
+                temporaryPosition1 = currentPosition;
+                temporaryPosition2 = currentPosition;
+                temporaryPosition3 = currentPosition;
+                temporaryPosition4 = currentPosition;
+            }
+        }
+    }
 		
 }
--- TileLands/Assets/scripts/GameLoopScript.cs
+++ TileLands/Assets/scripts/GameLoopScript.cs
@@ -2,79 +2,200 @@
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine;
+using System.Linq;
 
 public class GameLoopScript : MonoBehaviour {
 
-	public static GameLoopScript instance;
+    public static GameLoopScript instance;
 
-	GameObject startTile;
-	GameObject sunLight;
-	GameObject ocean;
-	GameObject EnvironmentPlane;
-	GameObject gameCube;
+    GameObject sunLight;
+    GameObject ocean;
+    GameObject EnvironmentPlane;
+    GameObject gameCube;
+    GameObject firework;
 
-	public List<GameObject> solutionList;
-	public List<GameObject> activeCubeList;
-
-	float randomLightangle;
-
-	// Use this for initialization
-	void Start () {
+    bool solved;
+    bool fireworkTriggered = false;
+    bool fireworkFinished = false;
+    bool resettingMoves;
+    bool rotatingCamera;
 
 
-		//create instance of this object
-		instance = this;
+    int fireworkTimer;
+    float endingTimer;
+    float endgameCameraRotation;
 
-		//calculate random sunlight angle.
-		randomLightangle = (float)(Random.Range(-80, 81));
+    float cameraRotationValue;
 
-		//create the new solution list
-		solutionList = new List<GameObject> ();
+    public List<GameObject> solutionList;
+    public List<GameObject> activeCubeList;
 
-		//list of all active cubes
-		activeCubeList = new List<GameObject> ();
-	
+    float randomLightangle;
 
-		sunLight = (GameObject)Instantiate (Resources.Load ("Lights/SunLightObject"), Vector3.zero, Quaternion.identity);
-		sunLight.transform.Rotate(new Vector3( 90f, 0, 0));
+    // Use this for initialization
+    void Start() {
 
-		//create ocean;
-		ocean = Resources.Load<GameObject> ("OceanPlane");
-		//create ocean plane or other object.
-		EnvironmentPlane = (GameObject)Instantiate (ocean, Vector3.zero, Quaternion.identity);
+        endgameCameraRotation = 0f;
+        fireworkTimer = 0;
+        endingTimer = 0;
+        cameraRotationValue = 0;
 
-		gameCube = Resources.Load<GameObject> ("BasicTile");
+        resettingMoves = false;
+        rotatingCamera = false;
 
-		//create the new start tile
-		//startTile = (GameObject)(Instantiate (Resources.Load ("BasicTile"), Vector3.zero, Quaternion.identity));
+        //puzzle isnt solved
+        solved = false;
 
-		CreateCube (Vector3.zero);
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+        //create instance of this object
+        instance = this;
 
-		if (Input.GetKeyDown(KeyCode.R))
-			{
-				{
+        //calculate random sunlight angle.
+        randomLightangle = (float)(Random.Range(-80, 81));
 
-					//restart scene
-				ResetGameScene();
-				}
-			}
+        //create the new solution list
+        solutionList = new List<GameObject>();
 
-	}
-
-	public void ResetGameScene()
-	{
-		SceneManager.LoadScene("Tile Test Scene");
-	}
+        //list of all active cubes
+        activeCubeList = new List<GameObject>();
 
 
-	public void CreateCube(Vector3 position)
-	{
-		activeCubeList.Add(Instantiate(gameCube, position, Quaternion.identity));
-	}
+        sunLight = (GameObject)Instantiate(Resources.Load("Lights/SunLightObject"), Vector3.zero, Quaternion.identity);
+        sunLight.transform.Rotate(new Vector3(90f, 0, 0));
 
+        //create ocean;
+        ocean = Resources.Load<GameObject>("OceanPlane");
+        //create ocean plane or other object.
+        EnvironmentPlane = (GameObject)Instantiate(ocean, Vector3.zero, Quaternion.identity);
+
+        gameCube = Resources.Load<GameObject>("BasicTile");
+
+        //create the new start tile
+        //startTile = (GameObject)(Instantiate (Resources.Load ("BasicTile"), Vector3.zero, Quaternion.identity));
+
+        CreateCube(Vector3.zero);
+
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        //only check for solution if the game is active.
+        if (GameManagerScript.instance.gameActive)
+        {
+            //solution checker
+            solved = solutionList.All(s => activeCubeList.Any(a => a.transform.position == s.transform.position));
+        }
+
+        //temporary solve flag
+        if (solved)
+        {
+            //deactivate game
+            GameManagerScript.instance.gameActive = false;
+
+            endingTimer += .01f;
+
+            if (!fireworkTriggered)
+            {
+                //make a new firework
+                firework = (GameObject)Instantiate(Resources.Load("Effects/VictoryParticle"), Vector3.zero, Quaternion.identity);
+                fireworkTriggered = true;
+            }
+
+            if (endgameCameraRotation < .5f)
+            {
+                endgameCameraRotation += .01f;
+            }
+
+            GameManagerScript.instance.mainCamera.transform.Rotate(new Vector3(0, endgameCameraRotation, 0));
+
+            if (endingTimer > 4)
+            {
+                ResetGameScene();
+            }
+
+
+        }
+
+
+        if (GameManagerScript.instance.gameActive)
+        {
+            //to restart the game, use this command
+            //do input manager later
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                {
+
+                    //restart scene
+                    ResetGameScene();
+                }
+            }
+
+            //if enter key is pressed, all tiles are reset.
+            if (Input.GetKeyDown(KeyCode.U))
+            {
+                resettingMoves = true;
+            }
+
+            //for camera rotation.
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                rotatingCamera = true;
+            }
+
+            //enable the move resetter
+            if (resettingMoves)
+            {
+                ResetMoves();
+            }
+
+            //enable camera rotation
+            if (rotatingCamera)
+            {
+                RotateCamera();
+            }
+        }
+
+    }
+
+    public void ResetGameScene()
+    {
+        SceneManager.LoadScene("Tile Test Scene");
+    }
+
+
+    public void CreateCube(Vector3 position)
+    {
+        activeCubeList.Add(Instantiate(gameCube, position, Quaternion.identity));
+    }
+
+    public void ResetMoves()
+    {
+        foreach (GameObject gameObject in activeCubeList)
+        {
+            Destroy(gameObject);
+        }
+        activeCubeList.Clear();
+
+        //create new cube
+        CreateCube(Vector3.zero);
+
+        resettingMoves = false;
+    }
+
+    public void RotateCamera()
+    {
+
+        if (cameraRotationValue < 30)
+        {
+            cameraRotationValue += 1;
+            GameManagerScript.instance.mainCamera.transform.Rotate(new Vector3(0, 3 , 0));
+        }
+        else
+        {
+            cameraRotationValue = 0;
+            rotatingCamera = false;
+        }
+
+    }
 }
